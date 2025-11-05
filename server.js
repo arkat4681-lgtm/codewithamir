2940870675a765b05ea1362c53b2b8044d538aae
Amir Wazir <arkat4681@gmail.com>
2025-11-05 22:06:40 +0500

    Replaced bcrypt with bcryptjs for Render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const cors = require("cors");
 
